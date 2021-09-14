a4cb8fdfe0da4e2bb34808ae15e17b59a0bf78af
Guillermo A <duckbeardgd@gmail.com>
2021-09-14 20:14:56 +0200

    Unity project imported

---
+++ Hidden Project/Assets/Scripts/GlobalVariablesAndStrings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVariablesAndStrings : MonoBehaviour
+{
+    //---------------- MENU STRINGS ------------------------
+    public static  string MENU_NAME_LOADING = "loading";
+    public static string MENU_NAME_MAINMENU = "title";
+    public static string MENU_NAME_CREATEROOM = "createroom";
+    public static string MENU_NAME_ROOM = "room";
+    public static string MENU_NAME_ERRORMENU = "error";
+
+
+
+
+}
---
+++ Hidden Project/Assets/Scripts/Launcher.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text errorText;
+    [SerializeField] TMP_Text roomNameText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Connecting to Master");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        MenuManager.Instance.OpenMenu(GlobalVariablesAndStrings.MENU_NAME_MAINMENU);
+        Debug.Log("Joined Lobby!");
+    }
+    public void CreateRoom()
+    {
+        if (string.IsNullOrEmpty(roomNameInputField.text))
+        {
+            return;
+        }
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuManager.Instance.OpenMenu(GlobalVariablesAndStrings.MENU_NAME_LOADING);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        MenuManager.Instance.OpenMenu(GlobalVariablesAndStrings.MENU_NAME_ROOM);
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        errorText.text = ("Room Creation failed: " + message);
+        //Debug.Log("Error joining room");
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu(GlobalVariablesAndStrings.MENU_NAME_LOADING);
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu(GlobalVariablesAndStrings.MENU_NAME_MAINMENU);
+    }
+}
